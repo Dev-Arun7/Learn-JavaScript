6852899f65626c8db0a8aee852245c825c5ed555
Dev-Arun7 <123arunblakrishnan@gmail.com>
2024-05-21 16:44:17 +0530

    v1.70 math functions are added

---
+++ 08_math funtions/049_math_functions.js
@@ -0,0 +1,46 @@
+// Rounds a number to the nearest integer
+console.log(Math.round(10.2)); // 10
+
+// Calculates the square root of a number
+console.log(Math.sqrt(64)); // 8
+
+// Raises a number to the power of another number
+console.log(Math.pow(5, 2)); // 25
+
+// Returns the value of PI
+console.log(Math.PI); // 3.141592653589793
+
+// Returns the largest of the given numbers
+console.log(Math.max(10, 20, 3, 99, 34)); // 99
+
+// Returns the smallest of the given numbers
+console.log(Math.min(10, 20, 3, 99, 34)); // 3
+
+// Returns the absolute value of a number
+console.log(Math.abs(-15)); // 15
+
+// Rounds a number up to the next largest integer
+console.log(Math.ceil(10.2)); // 11
+
+// Rounds a number down to the largest integer less than or equal to a given number
+console.log(Math.floor(10.8)); // 10
+
+// Returns a random number between 0 (inclusive) and 1 (exclusive)
+console.log(Math.random()); // e.g., 0.123456789
+
+// Returns the natural logarithm (base e) of a number
+console.log(Math.log(10)); // 2.302585092994046
+
+// Returns the sine of a number (angle in radians)
+console.log(Math.sin(Math.PI / 2)); // 1
+
+// Returns the cosine of a number (angle in radians)
+console.log(Math.cos(Math.PI)); // -1
+
+// Returns the tangent of a number (angle in radians)
+console.log(Math.tan(Math.PI / 4)); // 1
+
+
+
+
+
